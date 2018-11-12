6d4ed22e8a084f0a700be622a251d88ec03d414c
quangChe <quang.n.che@gmail.com>
2018-11-12 10:57:05 -0800

    Feat: Added clean cache functionality
    A cleanCache() middleware was incorporated to ensure that routes can
    enable clearing of the cache to enable data retrieval from Mongo and
    updating of the cache after.

--- services/cache.js
+++ services/cache.js
@@ -4,32 +4,42 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(options = {}) {
   this._caching = true;
+  this._hashkey = JSON.stringify(options.key || '');
   return this;
 }
 
 mongoose.Query.prototype.exec = async function() {
   if (!this._caching) return exec.apply(this, arguments);
-    
-  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+
+  const key = JSON.stringify({
+    ...this.getQuery(),
     collection: this.mongooseCollection.name
-  }));
-  const cacheValue = await client.get(key);
+  })
+
+  const cacheValue = await client.hget(this._hashkey, key);
 
   if (cacheValue) {
     const doc = JSON.parse(cacheValue);
+    console.log('Getting from cache!');
 
-    Array.isArray(doc) 
+    return Array.isArray(doc) 
       ? doc.map(d => new this.model(d))
       : new this.model(doc);
   }
 
   const results = await exec.apply(this, arguments);
-  client.set(key, JSON.stringify(results), 'EX', 10);
+  client.hset(this._hashkey, key, JSON.stringify(results), 'EX', 10);
 
   return results; 
+}
+
+module.exports = {
+  clearHash(hashkey) {
+    client.del(JSON.stringify(hashkey));
+  }
 }
